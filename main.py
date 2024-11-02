59f220691e0be1697386a56ab0f38503168d3a1f
vl348 <vl348@cam.ac.uk>
2024-11-02 19:30:11 +0000

    universe

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import pygame as pg
 from enum import Enum
 import random
+from universe import Universe
 
 class ScreenType(Enum):
     Universe = 1
     SolarSystem = 2
     Planet = 3
 
-
-
 pg.init()
 
+def change_screen(screen_type, seed):
+    pass
 
 W, H = 1880, 1000
 display = pg.display.set_mode((W, H))
@@ -19,19 +20,22 @@ global_seed = 27
 
 random.seed(global_seed)
 
-active_screen = None
+universe_screen = Universe(ScreenType.Universe, global_seed, W, H, change_screen)
+universe_screen.setup()
+
+active_screen = universe_screen
 
 run = True
 while run:
     dt = clock.tick(60)
-    if active_screen is None:
-        continue
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
-        
-        active_screen.event_loop(event) 
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_q:
+                run = False
+        active_screen.event_loop(event, dt) 
 
     active_screen.game_loop()
 
--- universe.py
+++ universe.py
@@ -1,36 +1,31 @@
 import pygame as pg
 
-pg.init()
-
-font = pg.font.Font(None, 36)
-clock = pg.time.Clock()
-display = pg.display.set_mode((1920, 1080))
 
 class System:
-    def __init__(self, name, color, x, y, radius, screen):
-        self.name = name
+    def __init__(self, color, x, y, radius, screen):
         self.color = color
         self.x = x
         self.y = y
         self.radius = radius
         self.screen = screen
-        self.nSectorX = self.WIDTH//self.sector_size
-        self.nSectorY = self.HEIGHT//self.sector_size
-
+        
     def plot(self):
         pg.draw.circle(self.screen, self.color, (self.x, self.y), self.radius)
 
 class Universe:
-    def __init__(self, type, seed):
+    def __init__(self, type, seed, W, H, callback):
+        self.callback = callback
         self.type = type
-        self.WIDTH = 1920
-        self.HEIGHT = 1080
+        self.WIDTH = W
+        self.HEIGHT = H
         self.seed = seed
         self.sector_size = 36
         self.cX = 0
         self.cY = 0
         self.vel = 4
         self.universeScreen = pg.Surface((self.WIDTH, self.HEIGHT))
+        self.nSectorX = self.WIDTH//self.sector_size
+        self.nSectorY = self.HEIGHT//self.sector_size
         
         
 
@@ -44,7 +39,7 @@ class Universe:
 
 
 
-    def universeScreenSetup(self):
+    def setup(self):
         
         self.universeScreen.fill((0,0,0))
         for y in range(self.nSectorY):
@@ -53,46 +48,50 @@ class Universe:
                 sectorY = (self.cY//self.sector_size) + y 
                 px = sectorX*self.sector_size - self.cX
                 py = sectorY*self.sector_size - self.cY
-                self.seed = sectorY << 32 | abs(sectorX+360)
+                self.seed = sectorY << 16 | abs(sectorX+360)
                 if self.lehmer(0,15) == 0:
                     colour = (self.lehmer(0,256), self.lehmer(0,256), self.lehmer(0,256))
                     r = self.lehmer(5,(self.sector_size-1)//2)
                     new = System(colour, px+(self.sector_size//2), py+(self.sector_size//2), r, self.universeScreen)
                     new.plot()
-        coord = font.render(f'({self.cX},{self.cY})', False, (255,255,255))
-        self.universeScreen.blit(coord, (0,0))
 
         
 
     def event_loop(self, event, dt):
-        if event.type == pg.KEYDOWN:
-            # if event.key == pg.K_a
-            pass
+        if event.type == pg.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                print(event.pos)
+                cursor_x, cursor_y = event.pos
+                sectorX = (self.cX + cursor_x)//self.sector_size
+                sectorY = (self.cY + cursor_y)//self.sector_size
+                self.seed = sectorY << 16 | abs(sectorX+360)
+                if self.lehmer(0,15) == 0:
+                    colour = (self.lehmer(0,256), self.lehmer(0,256), self.lehmer(0,256))
+                    print(colour)
     
     def game_loop(self):
         keys = pg.key.get_pressed()
+        tempVel = self.vel
+
         if keys[pg.K_LSHIFT]:
             tempVel = self.vel*2
         elif keys[pg.K_LCTRL]:
             tempVel = self.vel//4
         if keys[pg.K_w]:
             self.cY -= tempVel
-            self.universeScreenSetup()
+            self.setup()
         if keys[pg.K_a]:
             self.cX -= tempVel
-            self.universeScreenSetup()
+            self.setup()
         if keys[pg.K_s]:
             self.cY += tempVel
-            self.universeScreenSetup()
+            self.setup()
         if keys[pg.K_d]:
             self.cX += tempVel
-            self.universeScreenSetup()
-
-        pg.quit()
-        quit()
+            self.setup()
 
     def get_surface(self) -> pg.Surface:
-        pass
+        return self.universeScreen
 
 
 
